0c196c077cf5286094cd3cb027c665a750f58a74
Toni Spets <toni.spets@iki.fi>
2017-08-06 12:43:34 +0300

    Re-indent qcc, fix warnings

--- compiler/cmdlib.c
+++ compiler/cmdlib.c
@@ -1,21 +1,21 @@
 /*  Copyright (C) 1996-1997  Id Software, Inc.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-    See file, 'COPYING', for details.
-*/
+   See file, 'COPYING', for details.
+ */
 // cmdlib.c
 
 #include "cmdlib.h"
@@ -27,210 +27,195 @@
 int myargc;
 char **myargv;
 
-char	com_token[1024];
-int		com_eof;
+char com_token[1024];
+int com_eof;
 
 /*
-================
-I_FloatTime
-================
-*/
-double I_FloatTime (void)
+   ================
+   I_FloatTime
+   ================
+ */
+double I_FloatTime(void)
 {
-	struct timeval tp;
-	struct timezone tzp;
-	static int		secbase;
+    struct timeval tp;
+    struct timezone tzp;
+    static int secbase;
 
-	gettimeofday(&tp, &tzp);
-	
-	if (!secbase)
-	{
-		secbase = tp.tv_sec;
-		return tp.tv_usec/1000000.0;
-	}
-	
-	return (tp.tv_sec - secbase) + tp.tv_usec/1000000.0;
+    gettimeofday(&tp, &tzp);
+
+    if (!secbase) {
+        secbase = tp.tv_sec;
+        return tp.tv_usec / 1000000.0;
+    }
+
+    return (tp.tv_sec - secbase) + tp.tv_usec / 1000000.0;
 }
 
 
 /*
-==============
-COM_Parse
+   ==============
+   COM_Parse
 
-Parse a token out of a string
-==============
-*/
-char *COM_Parse (char *data)
+   Parse a token out of a string
+   ==============
+ */
+char *COM_Parse(char *data)
 {
-	int		c;
-	int		len;
-	
-	len = 0;
-	com_token[0] = 0;
-	
-	if (!data)
-		return NULL;
-		
-// skip whitespace
-skipwhite:
-	while ( (c = *data) <= ' ')
-	{
-		if (c == 0)
-		{
-			com_eof = true;
-			return NULL;			// end of file;
-		}
-		data++;
-	}
-	
-// skip // comments
-	if (c=='/' && data[1] == '/')
-	{
-		while (*data && *data != '\n')
-			data++;
-		goto skipwhite;
-	}
-	
+    int c;
+    int len;
 
-// handle quoted strings specially
-	if (c == '\"')
-	{
-		data++;
-		do
-		{
-			c = *data++;
-			if (c=='\"')
-			{
-				com_token[len] = 0;
-				return data;
-			}
-			com_token[len] = c;
-			len++;
-		} while (1);
-	}
+    len = 0;
+    com_token[0] = 0;
 
-// parse single characters
-	if (c=='{' || c=='}'|| c==')'|| c=='(' || c=='\'' || c==':')
-	{
-		com_token[len] = c;
-		len++;
-		com_token[len] = 0;
-		return data+1;
-	}
+    if (!data)
+        return NULL;
 
-// parse a regular word
-	do
-	{
-		com_token[len] = c;
-		data++;
-		len++;
-		c = *data;
-	if (c=='{' || c=='}'|| c==')'|| c=='(' || c=='\'' || c==':')
-			break;
-	} while (c>32);
-	
-	com_token[len] = 0;
-	return data;
+    // skip whitespace
+  skipwhite:
+    while ((c = *data) <= ' ') {
+        if (c == 0) {
+            com_eof = true;
+            return NULL;        // end of file;
+        }
+        data++;
+    }
+
+    // skip // comments
+    if (c == '/' && data[1] == '/') {
+        while (*data && *data != '\n')
+            data++;
+        goto skipwhite;
+    }
+    // handle quoted strings specially
+    if (c == '\"') {
+        data++;
+        do {
+            c = *data++;
+            if (c == '\"') {
+                com_token[len] = 0;
+                return data;
+            }
+            com_token[len] = c;
+            len++;
+        } while (1);
+    }
+    // parse single characters
+    if (c == '{' || c == '}' || c == ')' || c == '(' || c == '\''
+        || c == ':') {
+        com_token[len] = c;
+        len++;
+        com_token[len] = 0;
+        return data + 1;
+    }
+    // parse a regular word
+    do {
+        com_token[len] = c;
+        data++;
+        len++;
+        c = *data;
+        if (c == '{' || c == '}' || c == ')' || c == '(' || c == '\''
+            || c == ':')
+            break;
+    } while (c > 32);
+
+    com_token[len] = 0;
+    return data;
 }
 
 
 
 
 /*
-================
-filelength
-================
-*/
-int filelength (int handle)
+   ================
+   filelength
+   ================
+ */
+int filelength(int handle)
 {
-	struct stat	fileinfo;
-    
-	if (fstat (handle,&fileinfo) == -1)
-	{
-		Error ("Error fstating");
-	}
+    struct stat fileinfo;
 
-	return fileinfo.st_size;
+    if (fstat(handle, &fileinfo) == -1) {
+        Error("Error fstating");
+    }
+
+    return fileinfo.st_size;
 }
 
-int tell (int handle)
+int tell(int handle)
 {
-	return lseek (handle, 0, SEEK_CUR);
+    return lseek(handle, 0, SEEK_CUR);
 }
 
-char *strupr (char *start)
+char *strupr(char *start)
 {
-	char	*in;
-	in = start;
-	while (*in)
-	{
-		*in = toupper(*in);
-		in++;
-	}
-	return start;
+    char *in;
+    in = start;
+    while (*in) {
+        *in = toupper(*in);
+        in++;
+    }
+    return start;
 }
 
-char *strlower (char *start)
+char *strlower(char *start)
 {
-	char	*in;
-	in = start;
-	while (*in)
-	{
-		*in = tolower(*in);
-		in++;
-	}
-	return start;
+    char *in;
+    in = start;
+    while (*in) {
+        *in = tolower(*in);
+        in++;
+    }
+    return start;
 }
 
 
 /*
-=============================================================================
+   =============================================================================
 
-						MISC FUNCTIONS
+   MISC FUNCTIONS
 
-=============================================================================
-*/
+   =============================================================================
+ */
 
 /*
-=================
-Error
+   =================
+   Error
 
-For abnormal program terminations
-=================
-*/
-void Error (char *error, ...)
+   For abnormal program terminations
+   =================
+ */
+void Error(char *error, ...)
 {
-	va_list argptr;
+    va_list argptr;
 
-	printf ("\n************ ERROR ************\n");
+    printf("\n************ ERROR ************\n");
 
-	va_start (argptr,error);
-	vprintf (error,argptr);
-	va_end (argptr);
-	printf ("\n");
-	exit (1);
+    va_start(argptr, error);
+    vprintf(error, argptr);
+    va_end(argptr);
+    printf("\n");
+    exit(1);
 }
 
 
 /*
-=================
-CheckParm
+   =================
+   CheckParm
 
-Checks for the given parameter in the program's command line arguments
-Returns the argument number (1 to argc-1) or 0 if not present
-=================
-*/
-int CheckParm (char *check)
+   Checks for the given parameter in the program's command line arguments
+   Returns the argument number (1 to argc-1) or 0 if not present
+   =================
+ */
+int CheckParm(char *check)
 {
-	int             i;
+    int i;
 
-	for (i = 1;i<myargc;i++)
-	{
-		if ( !strcasecmp(check, myargv[i]) )
-			return i;
-	}
+    for (i = 1; i < myargc; i++) {
+        if (!strcasecmp(check, myargv[i]))
+            return i;
+    }
 
-	return 0;
+    return 0;
 }
 
 
@@ -238,379 +223,378 @@ int CheckParm (char *check)
 #define O_BINARY 0
 #endif
 
-int SafeOpenWrite (char *filename)
+int SafeOpenWrite(char *filename)
 {
-	int     handle;
+    int handle;
 
-	umask (0);
-	
-	handle = open(filename,O_WRONLY | O_CREAT | O_TRUNC | O_BINARY
-	, 0666);
+    umask(0);
 
-	if (handle == -1)
-		Error ("Error opening %s: %s",filename,strerror(errno));
+    handle = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0666);
 
-	return handle;
+    if (handle == -1)
+        Error("Error opening %s: %s", filename, strerror(errno));
+
+    return handle;
 }
 
-int SafeOpenRead (char *filename)
+int SafeOpenRead(char *filename)
 {
-	int     handle;
+    int handle;
 
-	handle = open(filename,O_RDONLY | O_BINARY);
+    handle = open(filename, O_RDONLY | O_BINARY);
 
-	if (handle == -1)
-		Error ("Error opening %s: %s",filename,strerror(errno));
+    if (handle == -1)
+        Error("Error opening %s: %s", filename, strerror(errno));
 
-	return handle;
-}
-
-
-void SafeRead (int handle, void *buffer, long count)
-{
-	if (read (handle,buffer,count) != count)
-		Error ("File read failure");
+    return handle;
 }
 
 
-void SafeWrite (int handle, void *buffer, long count)
+void SafeRead(int handle, void *buffer, long count)
 {
-	if (write (handle,buffer,count) != count)
-		Error ("File write failure");
+    if (read(handle, buffer, count) != count)
+        Error("File read failure");
 }
 
 
-void *SafeMalloc (long size)
+void SafeWrite(int handle, void *buffer, long count)
 {
-	void *ptr;
-
-	ptr = malloc (size);
-
-	if (!ptr)
-		Error ("Malloc failure for %lu bytes",size);
-
-	return ptr;
+    if (write(handle, buffer, count) != count)
+        Error("File write failure");
 }
 
 
-/*
-==============
-LoadFile
-==============
-*/
-long    LoadFile (char *filename, void **bufferptr)
+void *SafeMalloc(long size)
 {
-	int             handle;
-	long    length;
-	void    *buffer;
+    void *ptr;
 
-	handle = SafeOpenRead (filename);
-	length = filelength (handle);
-	buffer = SafeMalloc (length+1);
-	((byte *)buffer)[length] = 0;
-	SafeRead (handle, buffer, length);
-	close (handle);
+    ptr = malloc(size);
 
-	*bufferptr = buffer;
-	return length;
+    if (!ptr)
+        Error("Malloc failure for %lu bytes", size);
+
+    return ptr;
 }
 
 
 /*
-==============
-SaveFile
-==============
-*/
-void    SaveFile (char *filename, void *buffer, long count)
+   ==============
+   LoadFile
+   ==============
+ */
+long LoadFile(char *filename, void **bufferptr)
 {
-	int             handle;
+    int handle;
+    long length;
+    void *buffer;
 
-	handle = SafeOpenWrite (filename);
-	SafeWrite (handle, buffer, count);
-	close (handle);
-}
+    handle = SafeOpenRead(filename);
+    length = filelength(handle);
+    buffer = SafeMalloc(length + 1);
+    ((byte *) buffer)[length] = 0;
+    SafeRead(handle, buffer, length);
+    close(handle);
 
-
-
-void DefaultExtension (char *path, char *extension)
-{
-	char    *src;
-//
-// if path doesn't have a .EXT, append extension
-// (extension should include the .)
-//
-	src = path + strlen(path) - 1;
-
-	while (*src != PATHSEPERATOR && src != path)
-	{
-		if (*src == '.')
-			return;                 // it has an extension
-		src--;
-	}
-
-	strcat (path, extension);
-}
-
-
-void DefaultPath (char *path, char *basepath)
-{
-	char    temp[128];
-
-	if (path[0] == PATHSEPERATOR)
-		return;                   // absolute path location
-	strcpy (temp,path);
-	strcpy (path,basepath);
-	strcat (path,temp);
-}
-
-
-void    StripFilename (char *path)
-{
-	int             length;
-
-	length = strlen(path)-1;
-	while (length > 0 && path[length] != PATHSEPERATOR)
-		length--;
-	path[length] = 0;
-}
-
-void    StripExtension (char *path)
-{
-	int             length;
-
-	length = strlen(path)-1;
-	while (length > 0 && path[length] != '.')
-	{
-		length--;
-		if (path[length] == '/')
-			return;		// no extension
-	}
-	if (length)
-		path[length] = 0;
+    *bufferptr = buffer;
+    return length;
 }
 
 
 /*
-====================
-Extract file parts
-====================
-*/
-void ExtractFilePath (char *path, char *dest)
+   ==============
+   SaveFile
+   ==============
+ */
+void SaveFile(char *filename, void *buffer, long count)
 {
-	char    *src;
+    int handle;
 
-	src = path + strlen(path) - 1;
-
-//
-// back up until a \ or the start
-//
-	while (src != path && *(src-1) != PATHSEPERATOR)
-		src--;
-
-	memcpy (dest, path, src-path);
-	dest[src-path] = 0;
+    handle = SafeOpenWrite(filename);
+    SafeWrite(handle, buffer, count);
+    close(handle);
 }
 
-void ExtractFileBase (char *path, char *dest)
+
+
+void DefaultExtension(char *path, char *extension)
 {
-	char    *src;
+    char *src;
+    //
+    // if path doesn't have a .EXT, append extension
+    // (extension should include the .)
+    //
+    src = path + strlen(path) - 1;
 
-	src = path + strlen(path) - 1;
+    while (*src != PATHSEPERATOR && src != path) {
+        if (*src == '.')
+            return;             // it has an extension
+        src--;
+    }
 
-//
-// back up until a \ or the start
-//
-	while (src != path && *(src-1) != PATHSEPERATOR)
-		src--;
-
-	while (*src && *src != '.')
-	{
-		*dest++ = *src++;
-	}
-	*dest = 0;
+    strcat(path, extension);
 }
 
-void ExtractFileExtension (char *path, char *dest)
+
+void DefaultPath(char *path, char *basepath)
 {
-	char    *src;
+    char temp[128];
 
-	src = path + strlen(path) - 1;
+    if (path[0] == PATHSEPERATOR)
+        return;                 // absolute path location
+    strcpy(temp, path);
+    strcpy(path, basepath);
+    strcat(path, temp);
+}
 
-//
-// back up until a . or the start
-//
-	while (src != path && *(src-1) != '.')
-		src--;
-	if (src == path)
-	{
-		*dest = 0;	// no extension
-		return;
-	}
 
-	strcpy (dest,src);
+void StripFilename(char *path)
+{
+    int length;
+
+    length = strlen(path) - 1;
+    while (length > 0 && path[length] != PATHSEPERATOR)
+        length--;
+    path[length] = 0;
+}
+
+void StripExtension(char *path)
+{
+    int length;
+
+    length = strlen(path) - 1;
+    while (length > 0 && path[length] != '.') {
+        length--;
+        if (path[length] == '/')
+            return;             // no extension
+    }
+    if (length)
+        path[length] = 0;
 }
 
 
 /*
-==============
-ParseNum / ParseHex
-==============
-*/
-long ParseHex (char *hex)
+   ====================
+   Extract file parts
+   ====================
+ */
+void ExtractFilePath(char *path, char *dest)
 {
-	char    *str;
-	long    num;
+    char *src;
 
-	num = 0;
-	str = hex;
+    src = path + strlen(path) - 1;
 
-	while (*str)
-	{
-		num <<= 4;
-		if (*str >= '0' && *str <= '9')
-			num += *str-'0';
-		else if (*str >= 'a' && *str <= 'f')
-			num += 10 + *str-'a';
-		else if (*str >= 'A' && *str <= 'F')
-			num += 10 + *str-'A';
-		else
-			Error ("Bad hex number: %s",hex);
-		str++;
-	}
+    //
+    // back up until a \ or the start
+    //
+    while (src != path && *(src - 1) != PATHSEPERATOR)
+        src--;
 
-	return num;
+    memcpy(dest, path, src - path);
+    dest[src - path] = 0;
+}
+
+void ExtractFileBase(char *path, char *dest)
+{
+    char *src;
+
+    src = path + strlen(path) - 1;
+
+    //
+    // back up until a \ or the start
+    //
+    while (src != path && *(src - 1) != PATHSEPERATOR)
+        src--;
+
+    while (*src && *src != '.') {
+        *dest++ = *src++;
+    }
+    *dest = 0;
+}
+
+void ExtractFileExtension(char *path, char *dest)
+{
+    char *src;
+
+    src = path + strlen(path) - 1;
+
+    //
+    // back up until a . or the start
+    //
+    while (src != path && *(src - 1) != '.')
+        src--;
+    if (src == path) {
+        *dest = 0;              // no extension
+        return;
+    }
+
+    strcpy(dest, src);
 }
 
 
-long ParseNum (char *str)
+/*
+   ==============
+   ParseNum / ParseHex
+   ==============
+ */
+long ParseHex(char *hex)
 {
-	if (str[0] == '$')
-		return ParseHex (str+1);
-	if (str[0] == '0' && str[1] == 'x')
-		return ParseHex (str+2);
-	return atol (str);
+    char *str;
+    long num;
+
+    num = 0;
+    str = hex;
+
+    while (*str) {
+        num <<= 4;
+        if (*str >= '0' && *str <= '9')
+            num += *str - '0';
+        else if (*str >= 'a' && *str <= 'f')
+            num += 10 + *str - 'a';
+        else if (*str >= 'A' && *str <= 'F')
+            num += 10 + *str - 'A';
+        else
+            Error("Bad hex number: %s", hex);
+        str++;
+    }
+
+    return num;
+}
+
+
+long ParseNum(char *str)
+{
+    if (str[0] == '$')
+        return ParseHex(str + 1);
+    if (str[0] == '0' && str[1] == 'x')
+        return ParseHex(str + 2);
+    return atol(str);
 }
 
 
 
 /*
-============================================================================
+   ============================================================================
 
-					BYTE ORDER FUNCTIONS
+   BYTE ORDER FUNCTIONS
 
-============================================================================
-*/
+   ============================================================================
+ */
 
 #ifdef __BIG_ENDIAN__
 
-short   LittleShort (short l)
+short LittleShort(short l)
 {
-	byte    b1,b2;
+    byte b1, b2;
 
-	b1 = l&255;
-	b2 = (l>>8)&255;
+    b1 = l & 255;
+    b2 = (l >> 8) & 255;
 
-	return (b1<<8) + b2;
+    return (b1 << 8) + b2;
 }
 
-short   BigShort (short l)
+short BigShort(short l)
 {
-	return l;
-}
-
-
-long    LittleLong (long l)
-{
-	byte    b1,b2,b3,b4;
-
-	b1 = l&255;
-	b2 = (l>>8)&255;
-	b3 = (l>>16)&255;
-	b4 = (l>>24)&255;
-
-	return ((long)b1<<24) + ((long)b2<<16) + ((long)b3<<8) + b4;
-}
-
-long    BigLong (long l)
-{
-	return l;
+    return l;
 }
 
 
-float	LittleFloat (float l)
+long LittleLong(long l)
 {
-	union {byte b[4]; float f;} in, out;
-	
-	in.f = l;
-	out.b[0] = in.b[3];
-	out.b[1] = in.b[2];
-	out.b[2] = in.b[1];
-	out.b[3] = in.b[0];
-	
-	return out.f;
+    byte b1, b2, b3, b4;
+
+    b1 = l & 255;
+    b2 = (l >> 8) & 255;
+    b3 = (l >> 16) & 255;
+    b4 = (l >> 24) & 255;
+
+    return ((long) b1 << 24) + ((long) b2 << 16) + ((long) b3 << 8) + b4;
 }
 
-float	BigFloat (float l)
+long BigLong(long l)
 {
-	return l;
+    return l;
+}
+
+
+float LittleFloat(float l)
+{
+    union {
+        byte b[4];
+        float f;
+    } in, out;
+
+    in.f = l;
+    out.b[0] = in.b[3];
+    out.b[1] = in.b[2];
+    out.b[2] = in.b[1];
+    out.b[3] = in.b[0];
+
+    return out.f;
+}
+
+float BigFloat(float l)
+{
+    return l;
 }
 
 
 #else
 
 
-short   BigShort (short l)
+short BigShort(short l)
 {
-	byte    b1,b2;
+    byte b1, b2;
 
-	b1 = l&255;
-	b2 = (l>>8)&255;
+    b1 = l & 255;
+    b2 = (l >> 8) & 255;
 
-	return (b1<<8) + b2;
+    return (b1 << 8) + b2;
 }
 
-short   LittleShort (short l)
+short LittleShort(short l)
 {
-	return l;
+    return l;
 }
 
 
-long    BigLong (long l)
+long BigLong(long l)
 {
-	byte    b1,b2,b3,b4;
+    byte b1, b2, b3, b4;
 
-	b1 = l&255;
-	b2 = (l>>8)&255;
-	b3 = (l>>16)&255;
-	b4 = (l>>24)&255;
+    b1 = l & 255;
+    b2 = (l >> 8) & 255;
+    b3 = (l >> 16) & 255;
+    b4 = (l >> 24) & 255;
 
-	return ((long)b1<<24) + ((long)b2<<16) + ((long)b3<<8) + b4;
+    return ((long) b1 << 24) + ((long) b2 << 16) + ((long) b3 << 8) + b4;
 }
 
-long    LittleLong (long l)
+long LittleLong(long l)
 {
-	return l;
+    return l;
 }
 
-float	BigFloat (float l)
+float BigFloat(float l)
 {
-	union {byte b[4]; float f;} in, out;
-	
-	in.f = l;
-	out.b[0] = in.b[3];
-	out.b[1] = in.b[2];
-	out.b[2] = in.b[1];
-	out.b[3] = in.b[0];
-	
-	return out.f;
+    union {
+        byte b[4];
+        float f;
+    } in, out;
+
+    in.f = l;
+    out.b[0] = in.b[3];
+    out.b[1] = in.b[2];
+    out.b[2] = in.b[1];
+    out.b[3] = in.b[0];
+
+    return out.f;
 }
 
-float	LittleFloat (float l)
+float LittleFloat(float l)
 {
-	return l;
+    return l;
 }
 
 
 
 #endif
-
--- compiler/cmdlib.h
+++ compiler/cmdlib.h
@@ -1,21 +1,21 @@
 /*  Copyright (C) 1996-1997  Id Software, Inc.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-    See file, 'COPYING', for details.
-*/
+   See file, 'COPYING', for details.
+ */
 // cmdlib.h
 
 #ifndef __CMDLIB__
@@ -31,13 +31,16 @@
 #include <sys/file.h>
 #include <stdarg.h>
 
+#include <sys/types.h>
+#include <unistd.h>
+
 #ifdef NeXT
 #include <libc.h>
 #endif
 
 #ifndef __BYTEBOOL__
 #define __BYTEBOOL__
-typedef enum {false, true} boolean;
+typedef enum { false, true } boolean;
 typedef unsigned char byte;
 #endif
 
@@ -49,48 +52,48 @@ typedef unsigned char byte;
 extern int myargc;
 extern char **myargv;
 
-char *strupr (char *in);
-char *strlower (char *in);
-int filelength (int handle);
-int tell (int handle);
+char *strupr(char *in);
+char *strlower(char *in);
+int filelength(int handle);
+int tell(int handle);
 
-double I_FloatTime (void);
+double I_FloatTime(void);
 
-void	Error (char *error, ...);
-int		CheckParm (char *check);
+void Error(char *error, ...);
+int CheckParm(char *check);
 
-int 	SafeOpenWrite (char *filename);
-int 	SafeOpenRead (char *filename);
-void 	SafeRead (int handle, void *buffer, long count);
-void 	SafeWrite (int handle, void *buffer, long count);
-void 	*SafeMalloc (long size);
+int SafeOpenWrite(char *filename);
+int SafeOpenRead(char *filename);
+void SafeRead(int handle, void *buffer, long count);
+void SafeWrite(int handle, void *buffer, long count);
+void *SafeMalloc(long size);
 
-long	LoadFile (char *filename, void **bufferptr);
-void	SaveFile (char *filename, void *buffer, long count);
+long LoadFile(char *filename, void **bufferptr);
+void SaveFile(char *filename, void *buffer, long count);
 
-void 	DefaultExtension (char *path, char *extension);
-void 	DefaultPath (char *path, char *basepath);
-void 	StripFilename (char *path);
-void 	StripExtension (char *path);
+void DefaultExtension(char *path, char *extension);
+void DefaultPath(char *path, char *basepath);
+void StripFilename(char *path);
+void StripExtension(char *path);
 
-void 	ExtractFilePath (char *path, char *dest);
-void 	ExtractFileBase (char *path, char *dest);
-void	ExtractFileExtension (char *path, char *dest);
+void ExtractFilePath(char *path, char *dest);
+void ExtractFileBase(char *path, char *dest);
+void ExtractFileExtension(char *path, char *dest);
 
-long 	ParseNum (char *str);
+long ParseNum(char *str);
 
-short	BigShort (short l);
-short	LittleShort (short l);
-long	BigLong (long l);
-long	LittleLong (long l);
-float	BigFloat (float l);
-float	LittleFloat (float l);
+short BigShort(short l);
+short LittleShort(short l);
+long BigLong(long l);
+long LittleLong(long l);
+float BigFloat(float l);
+float LittleFloat(float l);
 
 
-char *COM_Parse (char *data);
+char *COM_Parse(char *data);
 
-extern	char	com_token[1024];
-extern	int		com_eof;
+extern char com_token[1024];
+extern int com_eof;
 
 
 
